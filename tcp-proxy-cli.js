e5686d7ec6cc1f30ad05594bde1edae5cc4cfd43
Devendra Tewari <devendra.tewari@gmail.com>
2018-06-26 13:32:21 -0300

    #3 switch to commander

--- tcp-proxy-cli.js
+++ tcp-proxy-cli.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 
-var argv = require("optimist")
-    .usage("Usage: $0 --proxyPort [port] [--hostname [IP]]"
-    + " --serviceHost [host] --servicePort [port] [--q]")
-    .demand(["proxyPort", "serviceHost", "servicePort"])
-	.boolean("q")
-    .argv;
+var argv = require("commander");
 
+argv
+    .usage("Usage: $0 --proxyPort [port] [--hostname [IP]]  --serviceHost [host] --servicePort [port] [--q]")
+    .usage("[options]")
+    .version("0.0.9")
+    .option("-p, --proxyPort <number>", "Proxy port number", parseInt)
+    .option("-h, --hostname [name]", "Name or IP address of host")
+    .option("-n, --serviceHost <name>", "Name or IP address of service host")
+    .option("-s, --servicePort <number>", "Service port number", parseInt)
+    .option("-q, --q", "Be quiet")
+    .parse(process.argv);
+console.log(argv);
 var options = {
     hostname: argv.hostname,
 	quiet: argv.q
